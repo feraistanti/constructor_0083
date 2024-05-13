3d82891a407b848c91969c414163aabd90a23d7b
feraistanti <fistanti551@gmail.com>
2024-05-13 15:15:08 +0700

    memasukkan int mahasiswa

--- mbrStatikfunc/mbrStatikfunc.cpp
+++ mbrStatikfunc/mbrStatikfunc.cpp
@@ -4,5 +4,23 @@ using namespace std;
 
 class mahasiswa
 {
+private:
+	static int nim;
 
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printALL();
+
+	static void setNim(int pNim) { nim = pNim; }
+	static int getNim() { return nim; }
+
+	mahasiswa(string pnama) :nama(pnama)
+	{
+		setID;
+	}
 };
+
+int mahasiswa::nim = 100;
