3e86c41f2384bf41b04d1328e7b07d55a55658da
feraistanti <fistanti551@gmail.com>
2024-05-13 14:50:50 +0700

    modifier public

--- constructor_0083/constructor_0083.cpp
+++ constructor_0083/constructor_0083.cpp
@@ -1,3 +1,34 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+class mahasiswa
+{
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+
+	void printALL();
+	mahasiswa(string pnama) :nama(pnama)
+	{
+		nama = pnama;
+		setID();
+	}
+};
+
+int mahasiswa::nim = 20;
+
+void mahasiswa::setID()
+{
+	id = nim++;
+}
+
+void mahasiswa::printALL()
+{
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
