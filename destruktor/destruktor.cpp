2dad4fdb786f200d9947ed890536690dc324310b
feraistanti <fistanti551@gmail.com>
2024-05-13 17:47:01 +0700

    memasukkan angka pada array dan int

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -1,2 +1,26 @@
 #include <iostream>
 using namespace std;
+
+class angka {
+private:
+	int* arr;
+	int panjang;
+public:
+	angka(int);
+	~angka();
+	void cetakData();
+	void isiData();
+};
+
+angka::angka(int i) {
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
+
+angka::~angka() {
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array sudah Dilepaskan" << endl;
+}
